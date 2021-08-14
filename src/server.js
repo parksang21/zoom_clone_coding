255e7ebed0949a87f6e597e35881a4775ce3d620
박상욱 <sanguk@bagsang-ug-ui-MacBookPro.local>
2021-08-14 16:24:51 +0900

    #0.3

--- src/server.js
+++ src/server.js
@@ -2,6 +2,17 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+// console.log("hello");
+// 이 아래에 있는 코드들은 설정을 위한 코드, 
+// view engine 설정하고
+app.set('view engine', 'pug')
+// views directory 설정하는 것.
+app.set('views', __dirname + "/views");
+// creating public url
+app.use("/public", express.static(__dirname + "/public"));
 
-app.listen(3000);
+app.get("/", (req, res) => res.render("home"));
+
+const handleListen = () => console.log('Listening on http://localhost:3000')
+
+app.listen(3000, handleListen);
